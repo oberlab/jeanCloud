45dda5d62a49c0ce9ee1ef5f96875d67db353338
Alexander Kutschera <alexander.kutschera@gmail.com>
2021-06-29 21:38:30 +0200

    fixed error in webserver

--- code/firmware/src/Webserver.cpp
+++ code/firmware/src/Webserver.cpp
@@ -1,18 +1,19 @@
 #include "Arduino.h"
 #include "Webserver.h"
+#include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 
-String processor(const String& var) {  
+
+String processor(const String& var) {
   return String();
 }
 
-Webserver::Webserver() {
-    server(80);
+Webserver::Webserver():server(80) {
 }
 
 void Webserver::setup() {
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", "<html><head><title>Jean Cloud</title></head></html>", processor);
+    request->send_P(200, "text/html", "<html><head><title>Jean Cloud</title></head> <body><h3> Hello You </h3> </body></html>", processor);
   });
 }
 
